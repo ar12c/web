6c93df08c7fe19d0096b0beec1d5c3d2a9b0c753
ar12c <s20221026@twghwfns.edu.hk>
2025-10-28 20:03:51 +0800

    Preparations

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeInOnce: {
+          '0%': { opacity: '0', transform: 'scale(0.98)', filter: 'blur(10px)' },
+          '100%': { opacity: '1', transform: 'scale(1)', filter: 'blur(0px)' },
+        },
+      },
+      animation: {
+        fadeInOnce: 'fadeInOnce 1s ease-out forwards',
+      },
+    },
+  },
+  plugins: [],
+}
